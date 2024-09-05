1a9b17c46d42be98446cc61dfc8a00da5e1f9401
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-09-05 14:15:51 +0200

    Nav
    Kleine 'proef' navigatie toegevoegd om naar squadpage te komen.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/squadpage">squadpage</a>
+</nav>
+
+<h1>home</h1>
+<p>this is the home page.</p>
+
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
---
+++ src/routes/squadpage/+page.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/squadpage">Squadpage</a>
+</nav>
+
+<h1>squadpage</h1>
+<p>this is the squadpage page.</p>
