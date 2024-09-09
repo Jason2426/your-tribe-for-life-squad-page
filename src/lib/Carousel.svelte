2068afab7d3003afbbcec2cc1c73e6a596fbb544
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-09 23:01:36 +0200

    CW - updated carousel
    - passed data
    - added previous/ next person logic

--- src/lib/Carousel.svelte
+++ src/lib/Carousel.svelte
@@ -1,17 +1,87 @@
 <script>
 	
 	export let people;
+	export let peep
 </script>
 
-
-{#each people as person}
-	<!-- <Card /> -->
-	<div>
-		<p>{person.name}</p>
+<h1>Im a carousel</h1>
+<section>
+	<div class="prev button">
+		<button>prev btn</button>
 	</div>
-{/each}
+	<ul>
+		{#each people as person}
+		<!-- <Card /> -->
+			{#if person.id === peep - 1}
+				<li>
+					<p>{person.name}</p>
+					<p>{person.squad_id}</p>
+				</li>
+			{/if}
+			{#if person.id === peep}
+				<li>
+					<p>{person.name}</p>
+					<p>{person.squad_id}</p>
+				</li>
+			{/if}
+			{#if person.id === peep + 1}
+				<li>
+					<p>{person.name}</p>
+					<p>{person.squad_id}</p>
+				</li>
+			{/if}
+			{#if person.id === peep + 2}
+				<li>
+					<p>{person.name}</p>
+					<p>{person.squad_id}</p>
+				</li>
+			{/if}
+		{/each}		
+	</ul>
+	<div class="next button">
+		<button>next btn</button>
+	</div>
+</section>
+
 
 <style>
+
+*,
+*::before,
+*::after,
+html {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  appearance: none;
+  text-decoration: none;
+  scroll-behavior: smooth;
+  scrollbar-gutter: stable;
+  scrollbar-width: thin;
+}
+
+	section{
+		/* background-color: #f0f0f0; */
+		width: 100%;
+		height: 100%;
+		padding-inline:3% ;
+		display: flex;
+		outline: solid;
+	}
+
+	ul{
+		display: flex;
+		justify-content: space-around;
+		gap: 3%;
+		width: 100%;
+	}
+	li{
+		outline: 1px solid;
+		width: 100%;
+		align-content: center;
+		background-color: #f0f0f0;
+	}
 
 	p {
 		color: red;
