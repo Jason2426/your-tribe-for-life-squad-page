3ee50c9c75e6e05be3a3e80b5248fe00e41a4f49
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-09 23:04:11 +0200

    CW - detail component
    - styling changes
    - rendering person data

--- src/routes/test-page/+page.svelte
+++ src/routes/test-page/+page.svelte
@@ -4,10 +4,16 @@
     /** @type {import('./$types').PageData} */
     export let data;
 
-    let showModal = false;
+    let showModal = true;
     const toggle = () =>{
 		showModal = !showModal;
 	}
+
+	console.log(data.people[0].id === 29);
+
+	let people = data.people;
+	
+	
     // Check if the data has been received and is an array
     // console.log("Received data in +page.svelte:", data);
 </script>
@@ -17,37 +23,86 @@
 
 {#if 10 > 2}
 	<p>10 bigger</p>
-	<Details showModal={showModal} on:click={toggle}>
-		<section>
-			<article>
-				<ul>
-					<li>name</li>
-					<li>age</li>
-					<li>fun fact</li>
-				</ul>
-			</article>
-			<article>
-				<h1>Details img</h1>
-				<img src="" alt="">
+	{#each people as person }
+		
+		{#if person.id === 44}
+		<Details showModal={showModal} on:click={toggle}>
 
-			</article>
-			<article>
-				<div>
-					<p>iframe github or other</p>
-					<iframe src="" frameborder="0"></iframe>
+			<section>
+				<article>
+					<ul>
+						<li>name : {person.name}</li>
+						<li>squad_id :{person.squad_id}</li>
+						<li>fun fact {person.bio}</li>
+					</ul>
+				</article>
+				<article>
+					<h1>Details img</h1>
+					<img src="{person.avatar}" alt="{person.name}-{person.squad_id}">
+
+				</article>
+				<article>
+					<div>
+						<p>iframe github or other</p>
+						<iframe src={person.website}  frameborder="0"></iframe>
+						<a href={person.website}>website</a>
+
+					</div>
+				</article>
+			</section>
+			<section>
+				<div class="carousel">
+					<Carousel people={data.people} peep={person.id} />
 				</div>
-			</article>
-		</section>
-	</Details>
+			</section>
+		</Details>
 
-	<Carousel people={data.people}/>
+		{/if}	
+	{/each}
+	
 {/if}
 
 <button on:click={toggle} > open modal
 
 </button>
 
+<style>
+*,
+*::before,
+*::after,
+html {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  appearance: none;
+  text-decoration: none;
+  scroll-behavior: smooth;
+  scrollbar-gutter: stable;
+  scrollbar-width: thin;
+}
 
+	section{
+		display: flex;
+	}
+
+	article{
+		flex: 1;
+	}
+	img{
+		width: 100%;
+	}
+
+	iframe{
+		width: 100%;
+	}
+	
+	.carousel{
+		width: 100%;
+		height: min-content;
+		overflow:scroll;
+	}
+</style>
 
 
 
