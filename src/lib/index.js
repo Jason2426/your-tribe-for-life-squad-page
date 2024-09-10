a3093f9e3b8b41f910e8e0aa719dc844e00000fd
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-09-10 16:02:20 +0200

    CW - top bar lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Details } from './Details.svelte'
 export { default as Carousel } from './Carousel.svelte'
+export { default as Topbar } from './Topbar.svelte'
